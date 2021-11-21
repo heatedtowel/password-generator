0b08f1df241984d0101944cdea028e2166f74c58
Jesse Monks <monksjesse@gmail.com>
2021-11-21 15:26:46 -0500

    logic fixes

--- Assets/script.js
+++ Assets/script.js
@@ -15,31 +15,28 @@ var choice4 = false
   
 
 //Options of password choices
-
 var options = 
   {
     lower: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    lownum: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9'],
+    uplow: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+    lownum: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'],
     lowsym: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
-    upandlow: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    lownumsym: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
+    lownumsym: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
     upper: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-    upnum: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9'],
-    upsym: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
-    uplownum: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9'],
-    uplowsym: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    upnumsym: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
-    all: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"']
+    upnum: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'],
+    upsym: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
+    uplownum: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'],
+    uplowsym: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
+    upnumsym: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"'],
+    all: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~','"']
   }
   
-  
-
 //Ensuring proper length before proceeding
 length = prompt('Please choose a password length between 8 and 124 characters.');
 
   if (length < 8) {
     return ('Please choose a password at least 8 characters long');
-  } else if (length > 124) {
+  } else if(length > 124) {
     return ('Please choose a password less than 125 characters');
   }  
 
@@ -47,45 +44,39 @@ length = prompt('Please choose a password length between 8 and 124 characters.')
 //Setting variables for logic
 var choice1 = confirm('Would you like to include lowercase letters?');
 var choice2 = confirm('Would you like to include uppercase letters?');
+
+if (choice1 == false && choice2 == false) {
+  return 'Your password must contain letters'
+}
+
 var choice3 = confirm('Would you like to include numbers?')
 var choice4 = confirm('Would you like to include symbols?')
 
 //Main logic
 if (choice1 == true && choice2 == true && choice3 == true && choice4 == true) {
   final = options.all
-} else if (choice1 == false && choice2 == false && choice3 == false && choice4 == false) {
-  return 'You must make at least one selection'
-
 } else if (choice1 == true && choice2 == false && choice3 == false && choice4 == false) {
   final = options.lower
 } else if (choice1 == false && choice2 == true && choice3 == false && choice4 == false) {
   final = options.upper 
-} else if (choice1 == false && choice2 == false && choice3 == true && choice4 == false) {
-  return 'Your password must contain letters'  
-} else if (choice1 == false && choice2 == false && choice3 == false && choice4 == true) {
-  return 'Your password must contain letters'  
-
 } else if (choice1 == false && choice2 == true && choice3 == true && choice4 == true) {
   final = options.upnumsym  
 } else if (choice1 == true && choice2 == false && choice3 == true && choice4 == true) {
   final = options.lownumsym  
 } else if (choice1 == true && choice2 == true && choice3 == false && choice4 == true) {
   final = options.uplowsym
-} else if (choice1 == true && choice2 == true && choice3 == true && choice4 == false) {
-  final = options.uplownum
-
-} else if (choice1 == true && choice2 == true && choice3 == false && choice4 == false) {
-  final = options.upandlow
 } else if (choice1 == false && choice2 == true && choice3 == true && choice4 == false) {
   final = options.upnum
-} else if (choice1 == false && choice2 == false && choice3 == true && choice4 == true) {
-  return 'Your password must contain letters'
-} else if (choice1 == false && choice2 == false && choice3 == true && choice4 == true) {
-  return 'Your password must contain letters'
+} else if (choice1 == true && choice2 == true && choice3 == true && choice4 == false) {
+  final = options.uplownum
+} else if (choice1 == true && choice2 == true && choice3 == false && choice4 == false) {
+  final = options.uplow
+} else if (choice1 == true && choice2 == false && choice3 == true && choice4 == false) {
+  final = options.lownum
 } else if (choice1 == true && choice2 == false && choice3 == false && choice4 == true) {
   final = options.lowsym
-} else if (choice1 == true && choice2 == true && choice3 == false && choice4 == false) {
-  final = options.upandlow
+} else if (choice1 == false && choice2 == true && choice3 == false && choice4 == true) {
+  final = options.upsym
 }
 
 //Building the password
@@ -99,7 +90,6 @@ if (choice1 == true && choice2 == true && choice3 == true && choice4 == true) {
   return str
   
 }
-
 
 
 
